86cd17325fdb9258f49e386e38d6da37d09cf162
hellboy95 <ajaypal95@gmail.com>
2019-10-11 20:11:44 -0700

    Creating shortURL redirect to long URL processor in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,24 @@
 const express = require('express');
 const router = express.Router();
 
+const Url = require('../model/Url');
 // GET /:code - Sending only the URL and no request ( Why we are using GET method)
 // @DESC Going to redirect the short code url to the respectice long codeurl in the db
+
+router.get('/:code', async (req,res) => {
+  //Check if the short code in the request is matching with any short code in the DB.
+  try{
+    const url = await Url.findOne({ urlCode: req.params.code })
+
+    if(url){
+      // See if return is required - test result: Not required
+      return res.redirect(url.urlLong);
+    }else {
+      return res.status(401).json('Short Url not found');
+    }
+  }catch(err){
+    console.log(err);
+    res.statis(500).json('Server Down!');
+  }
+})
 module.exports = router;
